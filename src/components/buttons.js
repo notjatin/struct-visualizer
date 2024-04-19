b50bebbc3fe2b650f9135c2ce12c5c522570117f
notjatin <kineticenergy96@outlook.com>
2024-04-19 06:39:31 +0530

    dev branch inits

--- src/components/buttons.js
+++ src/components/buttons.js
@@ -3,25 +3,30 @@ export const buttons = [
     label: "insert",
     info: "insert a value in the array.",
     broken: false,
+    onClick: "handleInsert()",
   },
   {
     label: "delete",
     info: "delete a value from the array.",
     broken: false,
+    onClick: "handleDelete",
   },
   {
     label: "find",
     info: "find a value in the array.",
     broken: true,
+    onClick: "handleFind",
   },
   {
     label: "sum",
     info: "sum the value(s) in the array.",
     broken: true,
+    onClick: "handleSum",
   },
   {
     label: "sort",
     info: "insert a value in the array.",
     broken: true,
+    onClick: "handleSort",
   },
 ];
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,17 +1,15 @@
 import { useState } from "react";
-import { buttons } from "./buttons";
-import RenderButtons from "./RenderButtons";
+import { capsFirst } from "./utilities/utils";
 import "./style.css";
 
 export default function MainScreen() {
   const [primaryArray, setPrimaryArray] = useState([]);
+
   return (
     <>
       <main className="main-container">
         <section className="output-screen"></section>
-        <section className="input-screen">
-          <RenderButtons buttons={buttons} />
-        </section>
+        <section className="input-screen"></section>
       </main>
     </>
   );
